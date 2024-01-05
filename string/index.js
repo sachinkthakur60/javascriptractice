670e8ab50ac93ffa30fdf65c774473db9136e228
Sachin K Thakur <60362013+sachinkthakur60@users.noreply.github.com>
2024-01-05 20:41:48 +0530

    Create index.js

---
+++ string/index.js
@@ -0,0 +1,13 @@
+const name="Amar"
+const repoCount = 50
+
+//traditional concatination
+console.log(name + repoCount + " value");
+
+// modern concatination this method is called as String Enterpoplation
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Sachin')
+
+console.log(gameName);
+console.log(gameName.anchor());
